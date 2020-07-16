a6cfb311c2fba50917fc8ae542f904edf82ac106
Vladimir Dudkin <vdudkin@lanapro.com>
2020-07-16 14:22:20 +0300

    back button image added

---
+++ GameCountHelper/UI/VC/HistoryViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HistoryViewController.swift
+//  GameCountHelper
+//
+//  Created by Vlad on 7/10/20.
+//  Copyright © 2020 Alexx. All rights reserved.
+//
+
+import UIKit
+
+class HistoryViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
